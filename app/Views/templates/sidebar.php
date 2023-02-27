1760662a31eac7d0eecbdc12c3fb449108ecbaa5
NaufalAjaSih <masanaufal@gmail.com>
2023-02-27 16:57:27 +0700

    first commit

---
+++ app/Views/templates/sidebar.php
@@ -0,0 +1,53 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+      <i class="fas fa-laugh-wink"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">Login</div>
+  </a>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider">
+
+  <!-- Heading -->
+  <div class="sidebar-heading">
+    User profile
+  </div>
+
+  <!-- Nav Item - Charts -->
+  <li class="nav-item">
+    <a class="nav-link" href="charts.html">
+      <i class="fas fa-fw fa-user"></i>
+      <span>My Profile</span></a>
+  </li>
+
+  <!-- Nav Item - Tables -->
+  <li class="nav-item">
+    <a class="nav-link" href="tables.html">
+      <i class="fas fa-fw fa-user-edit"></i>
+      <span>Edit Profile</span></a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider">
+
+  <li class="nav-item">
+    <a class="nav-link" href="tables.html">
+      <i class="fas fa-fw fa-sign-out-alt"></i>
+      <span>Logout</span></a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
+
+</ul>
+<!-- End of Sidebar -->
---
+++ app/Views/user/index.php
@@ -0,0 +1,8 @@
+<?= $this->extend('templates/index'); ?>
+
+
+<?= $this->section('content'); ?>
+<div class="container-fluid">
+  <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+</div>
+<?= $this->endSection(); ?>
